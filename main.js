397d3644a37a81e08e68868aa7434b28be95c76a
Bowo Adejuyigbe <bowo.adej@outlook.com>
2024-02-11 20:59:25 +0000

    adding rollup and cookie css

---
+++ main.js
@@ -0,0 +1,9 @@
+
+export default function CookieComponent {
+    return (
+        <div className="cookie-container">
+            <h1>This Site Uses Cookies</h1>
+            <p>This site uses cookies to collect analyse useage and customer information, to enhance the quality of the customer experience.</p>
+        </div>
+    )
+}
